6c437e932b9fa2835d694e5e36c112d9912ef5ca
Anish Kumar <anish@Anishs-MacBook-Pro.local>
2020-11-05 19:04:06 +0530

    added query param based get for images

--- src/resources/image/image.controller.js
+++ src/resources/image/image.controller.js
@@ -1,40 +1,40 @@
 import { Image } from "./image.model";
 import sharp from "sharp";
 
-const createOne = model => async (req, res) => {
+const createOne = (model) => async (req, res) => {
   try {
+    if (!req.file)
+      return res.status(400).json({
+        message:
+          "No image file has been recieved at our end! Please attach an image file",
+      });
     const { buffer, mimetype } = req.file;
     const doc = await model.create({
       title: req.body.title,
       user: req.user._id,
       image: {
         data: buffer,
-        contentType: mimetype
-      }
+        contentType: mimetype,
+      },
     });
     const processedImage = await sharp(buffer)
       .grayscale()
       .resize({ width: 100 })
       .toBuffer();
-    return res
-      .status(201)
-      .contentType(mimetype)
-      .send(processedImage);
+    return res.status(201).contentType(mimetype).send(processedImage);
   } catch (e) {
     console.error(e);
     res.status(400).json({
       message:
-        "Image with the same title is already created by you. Please change the title and try again!"
+        "Image with the same title is already created by you. Please change the title and try again!",
     });
   }
 };
 
-const getAll = model => async (req, res) => {
+const getAll = (model) => async (req, res) => {
   try {
-    const docs = await model
-      .find()
-      .lean()
-      .exec();
+    const queryParams = req.query;
+    const docs = await model.find(queryParams).lean().exec();
     res.status(201).json({ data: docs });
   } catch (e) {
     console.error(e);
@@ -42,4 +42,19 @@ const getAll = model => async (req, res) => {
   }
 };
 
-export default { createOne: createOne(Image), getAll: getAll(Image) };
+const getOne = (model) => async (req, res) => {
+  try {
+    const { id } = req.params;
+    const image = await model.findById(id).lean().exec();
+    res.status(201).json({ data: image });
+  } catch (e) {
+    console.error(e);
+    res.status(400).end();
+  }
+};
+
+export default {
+  createOne: createOne(Image),
+  getAll: getAll(Image),
+  getOne: getOne(Image),
+};
--- src/resources/image/image.router.js
+++ src/resources/image/image.router.js
@@ -4,9 +4,7 @@ import controller from "./image.controller";
 const router = Router();
 
 // /api/blog
-router
-  .route("/")
-  .post(controller.createOne)
-  .get(controller.getAll);
+router.route("/").post(controller.createOne).get(controller.getAll);
 
+router.route("/:id").get(controller.getOne);
 export default router;
