b014dbcc82dd9a860687e150d7d56dae2b51a650
Anish Kumar <anish@Anishs-MacBook-Pro.local>
2020-12-24 16:55:38 +0530

    basic socket setup alongwith express server

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from "express";
 import { connect } from "./db";
 import { json, urlencoded } from "body-parser";
 import morgan from "morgan";
-
 import multer from "multer";
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
@@ -12,8 +11,11 @@ import userRouter from "./resources/user/user.router";
 import authRouter from "./resources/auth/auth.router";
 
 import { protect } from "./resources/utils/auth";
+import { Socket } from "socket.io-client";
 
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const PORT = process.env.PORT || 3000;
 
 app.disable("x-powered-by");
@@ -26,14 +28,23 @@ app.use("/apis/users", userRouter);
 app.use("/apis", protect);
 app.use("/apis/images", upload.single("image"), imageRouter);
 
-app.get("/", async (req, res) => {
+app.get("/", async (_req, res) => {
   res.json({ text: "hey this is a json sent after connecting to db!!" });
+});
+
+io.on('connection', (socket:Socket) => { 
+  console.log("websocket connected");
+
+  socket.on("ping",(_name:String,fn:Function)=>{
+    fn("pong");
+  })
+
 });
 
 export async function start() {
   await connect();
   console.log("connected to db successfully!");
-  app.listen(PORT, () =>
-    console.log(`Example app listening at http://localhost:${PORT}`)
+  server.listen(PORT, () =>
+    console.log(`Simple Server listening at http://localhost:${PORT}`)
   );
 }
